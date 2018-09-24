3e4bb9bcbfe648fb1b934011c731500a81c5eae2
David Eliason <davethemaker@gmail.com>
2018-09-24 15:50:49 -0700

    add ejs template engine, pass returned db values to the engine view so that the quotes are displayed as list values

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
 require('dotenv').config()
+app.set('view engine', 'ejs')
 
 var db;
 
@@ -15,7 +16,11 @@ MongoClient.connect(process.env.DB_HOST, (err,client) => {
 	app.use(bodyParser.urlencoded({extended: true}));
 
 	app.get('/', (req,res) => {
-		res.sendFile(__dirname + '/index.html');
+		db.collection('quotes').find().toArray(function(err,result){
+			if(err) return console.log(err)
+				res.render('index.ejs', {quotes: result})
+		});
+
 	});
 
 	app.post('/quotes', (req, res) => {
