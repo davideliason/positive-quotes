cf0482bf4ce781bbc38dd7f12b76cb60ea55adb0
David Eliason <davethemaker@gmail.com>
2018-09-24 15:02:19 -0700

    add form for input new quote along with route for POST

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/quotes', (req,res) => {
+	res.send('got a new quote');
+});
+
 app.listen(3000, () => {
 	console.log("listening on port 3000");
 });
