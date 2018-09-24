7183bfd6b4749e130eb8fd5ac13db9e05d6e3139
David Eliason <davethemaker@gmail.com>
2018-09-24 15:25:58 -0700

    add mlab mongodb connectivity saving POSTed data to collection

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+require('dotenv').config()
+
+var db;
+
+MongoClient.connect(process.env.DB_HOST, (err,client) => {
+	console.log('connected to db');
+	if(err) return console.log(err);
+	db = client.db('positive-quotes');
+
+	app.use(bodyParser.urlencoded({extended: true}));
+
+	app.get('/', (req,res) => {
+		res.sendFile(__dirname + '/index.html');
+	});
+
+	app.post('/quotes', (req, res) => {
+  		db.collection('quotes').save(req.body, (err, result) => {
+    	if (err) return console.log(err)
+
+    	console.log('saved to database')
+    	res.redirect('/')
+  })
+})
+
+	app.listen(3000, () => {
+	console.log("listening on port 3000");
+   });
+});
 
 // middleware
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', (req,res) => {
-	res.sendFile(__dirname + '/index.html');
-});
-
-app.post('/quotes', (req,res) => {
-	console.log(req.body);
-	res.send('got a new quote');
-});
-
-app.listen(3000, () => {
-	console.log("listening on port 3000");
-});
