f07e90603758d45aac408b80381c291b3349b2bd
David Eliason <davethemaker@gmail.com>
2018-09-24 15:06:33 -0700

    add bodyparser for obtaining form data values in request body

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// middleware
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/quotes', (req,res) => {
+	console.log(req.body);
 	res.send('got a new quote');
 });
 
